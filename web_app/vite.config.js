31f710078d20f1a0c872c11f42ada312bdda2395
Hoog-v <hogeweyv@gmail.com>
2026-04-17 22:40:53 +0200

    fix the name in vite config

--- web_app/vite.config.js
+++ web_app/vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',
+  base: '/AED_Retrofit/',
 })
